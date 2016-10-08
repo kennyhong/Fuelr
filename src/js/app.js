86d7a2b47007c31204d5375dbe58ddede1c40d73
Julius Trojillo <TROJILLO@Juliuss-MacBook-Pro.local>
2016-10-08 19:49:59 -0400

    changed low gas milage indicator

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,32 @@ angular.module('ngi', [])
   }]);
 
 
+// Your code goes here
+
+gm.info.getVehicleConfiguration(function (data) {
+
+});
+
+var fuelID = gm.info.watchVehicleData(processData, watchDataFailureCallback, ["fuel_level"]);
+
+if(fuelID <= 25)
+{
+	<script type="text/javascript">
+		window.open ('warningPage','_self',false)
+	</script>
+}
+
+// gm.info.getVehicleData(processData, ["fuel_level"]);
+
+/*function processData(data) {
+  // called continuously as watched signals update
+  // NOTE: expect some `analog` signals to bounce
+ 	console.log("@@@@", data);
+}
+
+function watchDataFailureCallback() {
+
+	console.error("T~T");
+}
+*/
+
