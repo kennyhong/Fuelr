e69ba33d626b2f528d318cf85410282bf80846ce
Kenny Hong <k.hongjiahan@gmail.com>
2016-10-08 20:59:56 -0400

    Google maps hackeroonies

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,7 @@
 // Your code goes here
-
 angular.module('ngi', [])
   .controller('HelloWorldCtrl', ['$scope', '$timeout', function($scope, $timeout){
     $timeout(function(){
       $scope.vin = gm.info.getVIN();
     }, 1000);
   }]);
-
-
