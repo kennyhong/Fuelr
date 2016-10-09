0d81ec8e51a9264053cbe4fc903f28a2975142e9
Jonathan Li <jli227@uw.edu>
2016-10-09 03:53:57 -0400

    attempting to fix back button

--- src/js/app.js
+++ src/js/app.js
@@ -16,37 +16,50 @@ angular.module("ngi", ["ngRoute"])
           });
     })
     .controller('HomeCtrl', function($scope, $route, $location) {
-      console.log("in HomeCtrl")
-            var fuelID = gm.info.watchVehicleData(processData, watchDataFailureCallback, ["fuel_level"]);
-            console.log('watching');
-            function processData(data) {
-                console.log(data);
-                if (data.fuel_level <= 25) {
-                  console.log("ATTEMPTING TO REDIRECT TO /warning/" + data.fuel_level);
-                    $location.path('/warning/' + data.fuel_level);
-                    gm.info.clearVehicleData(fuelID);
-                    $scope.$apply();
-                }
-            }
-            
-            function watchDataFailureCallback() {
-                console.error("T~T");
-            }
-        })
-    .controller('WarningCtrl', function($scope, $routeParams) {
-      console.log("in WarningCtrl");
-      $scope.fuel = $routeParams.fuel;
-      console.log($routeParams);
-
-            var fuelID = gm.info.watchVehicleData(processData, watchDataFailureCallback, ["fuel_level"]);
-            console.log('watching');
-            function processData(data) {
-                console.log(data);
-                $scope.fuel = data.fuel_level;
+        var fuelID = gm.info.watchVehicleData(processData, watchDataFailureCallback, ["fuel_level"]);
+        function initializeCoordinates(position) {
+            var coords = {lat: position.coords.latitude, lng: position.coords.longitude};
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 17,
+                center: coords
+            });
+            var marker = new google.maps.Marker({
+                position: coords,
+                map: map
+            });
+        }
+        
+        gm.info.getCurrentPosition(initializeCoordinates, true);
+        
+        function processData(data) {
+            if (data.fuel_level <= 25) {
+                $location.path('/warning/' + data.fuel_level);
+                gm.info.clearVehicleData(fuelID);
                 $scope.$apply();
             }
+        }
             
-            function watchDataFailureCallback() {
-                console.error("T~T");
-            }
+        function watchDataFailureCallback() {
+            console.error("T~T");
+        }
+    })
+    .controller('WarningCtrl', function($scope, $routeParams) {
+        $scope.fuel = $routeParams.fuel;
+
+        var fuelID = gm.info.watchVehicleData(processData, watchDataFailureCallback, ["fuel_level"]);;
+        function processData(data) {
+            console.log(data);
+            $scope.fuel = data.fuel_level;
+            $scope.$apply();
+        }
+            
+        function watchDataFailureCallback() {
+            console.error("T~T");
+        }
+    })
+    .controller("FunctionsCtrl", function ($scope) {
+        $scope.processData = function (data) {
+            $scope.fuel = data.fuel_level;
+            $scope.$apply();
+        };
     });
