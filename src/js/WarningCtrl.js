4cfb04611a8f17094b3cc1ad1d7cdb589693babd
Jonathan Li <jli227@uw.edu>
2016-10-08 21:55:28 -0400

    refactoring code and attempting to fix ui routing

--- src/js/WarningCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('ngi').controller('WarningCtrl', ['$scope', '$route', '$routeParams', '$location',
-  function($scope, $route, $routeParams, $location) {
-    $scope.fuel = $routeParams.fuel;
-    console.log($scope.fuel);
-}]);
--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,38 @@
 // Your code goes here
 
-angular.module('ngi', ['ngRoute'])
-  .config(function($routeProvider) {
-    $routeProvider.when('/', {
-      templateUrl: '../homePage.html',
-      controller: 'HomeCtrl',
+var app = angular.module("ngi", ["ui.router"]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state("warning/:fuel", {
+                url: "/warning/:fuel",
+                templateUrl: "../views/warningPage.html",
+                controller: "HomeCtrl"
+            })
+            .state("home", {
+                url: "/",
+                templateUrl: "../views/homePage.html"
+            });
+        $urlRouterProvider.otherwise("/");
     })
-    .when('/warning/:fuel', {
-      templateUrl: '../warningPage.html',
-      controller: 'WarningCtrl'
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
-  });
-
-
-
-
-
+    .controller('HomeCtrl',
+        function($scope, $location) {
+            var fuelID = gm.info.watchVehicleData(processData, watchDataFailureCallback, ["fuel_level"]);
+            console.log('watching');
+            function processData(data) {
+                console.log(data);
+                if(data.fuel_level <= 25) {
+                    console.log('A');
+                    $location.path = '/warning/' + data.fuel_level;
+                }
+            }
+            
+            function watchDataFailureCallback() {
+                console.error("T~T");
+            }
+        })
+    .controller('WarningCtrl',
+        function($scope, $route, $routeParams, $location) {
+            $scope.fuel = $routeParams.fuel;
+            console.log($scope.fuel);
+        });
